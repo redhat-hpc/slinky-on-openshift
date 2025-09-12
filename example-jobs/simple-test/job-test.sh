34865b0ea53250bc9ba483a374215cdd1657b8e8
Jason Kincl <jkincl@redhat.com>
2025-09-12 16:39:11 -0400

    updating test job syntax for new ohpc

--- example-jobs/simple-test/job-test.sh
+++ example-jobs/simple-test/job-test.sh
@@ -4,18 +4,10 @@
 #SBATCH --ntasks-per-node=4
 #SBATCH --exclusive
 
-# load Open MPI module
-eval `spack load --sh openmpi`
-
 set -x
 
-#mpirun -np 4 bash -c 'echo "Rank: $OMPI_COMM_WORLD_RANK, Hostname: $HOSTNAME"'
 # compile the C file
 mpicc test_mpi.c -o test_mpi
 
 # run compiled test_mpi.c file
-# srun -v -n 12 ./test_mpi
-
-# export OMPI_MCA_btl_tcp_if_exclude=eth0,lo
-# mpirun -np 12 -mca btl_base_verbose 100 ./test_mpi
-mpirun -np 8 ./test_mpi
+srun --mpi=pmix -n 8 ./test_mpi
